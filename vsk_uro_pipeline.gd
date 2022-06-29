3c1f7862232c51b3f24571208791ec0cb6d656a1
SaracenOne <SaracenOne@gmail.com>
2022-06-29 20:57:39 +0100

    Add default empty string to UroPipeline

--- vsk_uro_pipeline.gd
+++ vsk_uro_pipeline.gd
@@ -1,6 +1,6 @@
 extends "res://addons/vsk_importer_exporter/vsk_pipeline.gd" # vsk_pipeline.gd
 
-@export var database_id: String # (String) = ""
+@export var database_id: String = ""
 
 func _init(p_database_id: String):
 	database_id = p_database_id
